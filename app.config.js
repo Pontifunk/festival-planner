3c35c314172846d1e9445c70be61e1c70cfe033d
GEOINFO\TscM <michael.tschann@geoinfo.ch>
2026-02-17 16:29:04 +0100

    chore: sync app/sw build id to 2026-02-17-4

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@
 const DEFAULT_FESTIVAL = "tomorrowland";
 const DEFAULT_YEAR = "2026";
 const DEFAULT_WEEKEND = "W1";
-const BUILD_ID = "2026-02-17-1";
+const BUILD_ID = "2026-02-17-4";
 
 const DONATION_URL = "https://www.buymeacoffee.com/Pontifunk"; 
 const FEEDBACK_URL = "https://github.com/Pontifunk/festival-planner/issues/new/choose";
