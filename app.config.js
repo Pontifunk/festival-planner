98e00d4ac18b234e9453ef4c942be2204c12b5d8
GEOINFO\TscM <michael.tschann@geoinfo.ch>
2026-06-30 13:17:42 +0200

    Bump frontend cache version

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@
 const DEFAULT_FESTIVAL = "tomorrowland";
 const DEFAULT_YEAR = "2026";
 const DEFAULT_WEEKEND = "W1";
-const BUILD_ID = "2026-06-30-4";
+const BUILD_ID = "2026-06-30-5";
 
 const DONATION_URL = "https://www.buymeacoffee.com/Pontifunk"; 
 const FEEDBACK_URL = "https://github.com/Pontifunk/festival-planner/issues/new/choose";
